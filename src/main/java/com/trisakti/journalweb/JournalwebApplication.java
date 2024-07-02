7448d775b3ddaeb4da244568a12019e1c3a3f782
DimasHN_04 <dimashn04@gmail.com>
2024-07-02 17:38:49 +0700

    first commit

---
+++ src/main/java/com/trisakti/journalweb/JournalwebApplication.java
@@ -0,0 +1,13 @@
+package com.trisakti.journalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trisakti/journalweb/JournalwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trisakti.journalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
