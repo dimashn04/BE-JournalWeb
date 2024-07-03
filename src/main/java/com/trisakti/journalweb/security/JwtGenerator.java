4b5df955aa2814ee2883dce0f45ab2c29e6980aa
DimasHN_04 <dimashn04@gmail.com>
2024-07-03 23:39:49 +0700

    feat(api): JWT Token generator created

---
+++ src/main/java/com/trisakti/journalweb/security/JwtGenerator.java
@@ -0,0 +1,51 @@
+package com.trisakti.journalweb.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtGenerator {
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expiredDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION_TIME);
+
+        return Jwts.builder()
+            .setSubject(username)
+            .setIssuedAt(new Date())
+            .setExpiration(expiredDate)
+            .signWith(key, SignatureAlgorithm.HS512)
+            .compact();
+    }
+
+    public String getUsernameFromJwt(String token) {
+        Claims claims = Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
