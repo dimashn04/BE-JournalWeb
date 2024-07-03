7159d96be90f8ccee469bd0b714b88b8d12c546a
DimasHN_04 <dimashn04@gmail.com>
2024-07-03 23:41:02 +0700

    feat(api): Auth controllers for login and register done

---
+++ src/main/java/com/trisakti/journalweb/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.trisakti.journalweb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trisakti.journalweb.dto.AuthResponseDto;
+import com.trisakti.journalweb.dto.LoginDto;
+import com.trisakti.journalweb.dto.RegisterDto;
+import com.trisakti.journalweb.service.UserService;
+
+import jakarta.validation.Valid;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto) {
+        try {
+            AuthResponseDto response = userService.login(loginDto);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            AuthResponseDto response = new AuthResponseDto();
+            response.setToken(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterDto registerDto) {
+        try {
+            userService.create(registerDto);
+            return ResponseEntity.ok("User registered successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to register user!");
+        }
+    }
+}
